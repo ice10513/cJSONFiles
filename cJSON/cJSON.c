c45b2c8a56b7f88db83a24e151785b899c33ae94
ice <ice10513@126.com>
2018-10-15 20:22:06 +0800

    revert change if else

--- cJSON/cJSON.c
+++ cJSON/cJSON.c
@@ -59,17 +59,14 @@ static char* cJSON_strdup(const char* str)
 
 void cJSON_InitHooks(cJSON_Hooks* hooks)
 {
-    if (!hooks)/* Reset hooks */
-	{ 
+    if (!hooks) { /* Reset hooks */
         cJSON_malloc = malloc;
         cJSON_free = free;
         return;
     }
-	else
-	{
-		cJSON_malloc = (hooks->malloc_fn)?hooks->malloc_fn:malloc;
-		cJSON_free	 = (hooks->free_fn)?hooks->free_fn:free;
-	}
+
+	cJSON_malloc = (hooks->malloc_fn)?hooks->malloc_fn:malloc;
+	cJSON_free	 = (hooks->free_fn)?hooks->free_fn:free;
 }
 
 /* Internal constructor. */
